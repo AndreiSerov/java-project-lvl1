c2c82bcbfbb26e4f9d352b4fa0cab0ca70779de1
Серов Андрей <actsituation@gmail.com>
2021-10-24 12:51:11 +0700

    add printMenu logic remove scanner instance from Cli class

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,9 +1,24 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 import static hexlet.code.Cli.greet;
 
 public class App {
+    private static final Scanner SC = new Scanner(System.in);
+
     public static void main(String[] args) {
-        greet();
+        printMenu();
+
+        if (SC.nextInt() == 1) {
+            greet(SC);
+        }
+    }
+
+    private static void printMenu() {
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        System.out.println("Your choice:");
     }
 }
--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -3,11 +3,10 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    private static final Scanner SC = new Scanner(System.in);
 
-    public static void greet() {
+    public static void greet(Scanner sc) {
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
-        System.out.printf("Hello, %s!", SC.next());
+        System.out.printf("Hello, %s!", sc.next());
     }
 }
